47f4502ac75081646d27abfa899747df60e59a4d
Alexander Brill <alex@brill.no>
2018-05-14 14:52:08 +0200

    webpack library prod config

---
+++ vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+ chainWebpack: config => {
+   if (process.env.NODE_ENV === 'production') {
+    console.log('****************************')
+     console.log(config.plugins.entries())
+     config.plugins.delete('html')
+     config.plugins.delete('split-vendor')
+     config.plugins.delete('split-manifest')
+      config.plugins.get('extract-css')
+    .tap(args => {
+      args[0].disable = true
+      return args
+    })
+     config.output.libraryTarget('umd')
+     config.output.filename('js/app.js')
+
+    console.log('****************************')
+   }
+ }
+}
